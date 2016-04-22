1ff713c4e72ca4caa75ff08e2fded4123532dbf1
unknown <mmule@ManjushaM.clarice.com>
2016-04-22 15:47:27 +0530

    Inital template

---
+++ debt-ui/app/scripts/app.js
@@ -0,0 +1,82 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mytodoApp
+ * @description
+ * # mytodoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('DebtClearer', ['ngRoute', 'ui.router'])
+/*.config(function ($stateProvider,$urlRouterProvider,$routeProvider) {
+   $urlRouterProvider.when("", "/login");
+    $urlRouterProvider.when("/", "/login");
+    $urlRouterProvider.otherwise("/login");
+    $stateProvider
+      .state('login', {
+            url: '/login',
+            views: {
+
+          "MainView" : { templateUrl: 'views/balance.html',
+                            //controller: 'showBalanceCtrl'
+                          }//, 
+           
+           }
+            
+        })
+        .state('income', {
+            url: '/income',
+            views: {
+
+          "balanceView" : { templateUrl: 'views/balance.html',
+                            controller: 'showBalanceCtrl'
+                          }, 
+           "formView": {templateUrl: 'views/incomeForm.html',
+                        controller: 'IncomeCtrl'
+                        },
+            "displayView": { templateUrl: 'views/income.html',
+                              controller: 'showIncomeCtrl'
+                           }
+           }
+            
+        })
+         
+        .state('expense', {
+            url: '/expense',
+             views: {
+
+              "balanceView" : { templateUrl: 'views/balance.html',
+                                controller: 'showBalanceCtrl'
+                              }, 
+              "formView": { templateUrl: 'views/expenseForm.html',
+                           controller: 'ExpenseCtrl'
+                          },
+            "displayView": { templateUrl: 'views/expense.html',
+                              controller: 'showExpenseCtrl'
+                           }
+           }
+        });
+
+
+
+    $urlRouterProvider.otherwise('/login');
+
+})
+
+.config(function ($provide) {
+  $provide.provider("Balance", function () {
+    return {
+      $get: function () {
+        return {
+          total: {
+                  income : 0,
+                  expense : 0,
+                  balance : 0 
+                 }
+        };
+      }
+    };
+  });
+})*/
